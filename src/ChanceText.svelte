2c8beba91b662ebf26a76ac70f0b67051875ef97
Michal Surowka <mich@lsurowka.email>
2020-10-21 18:40:49 +0200

    forgot to include new files

---
+++ src/ChanceText.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let caption;
+    export let value;
+</script>
+
+<div style="margin-right: 5px; display: flex; flex-direction: column; justify-content: center; align-items: center;">
+    <span class="shadowtext robofont" style="font-size: 6px; text-align: center;">{caption}</span>
+    <span class="shadowtext" style="font-size: 24px; width: 100px; text-align: center;">{value}</span>
+</div>
